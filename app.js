62de58843beadbe8a490b5c12b2558939c958ebe
Aditya <rameshaditya2001@gmail.com>
2021-04-27 19:53:51 +0530

    Initial Webpage code updated

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('Javascript is working.');
+var results = "";
+$.get("http://127.0.0.1:6767/api/v1/resources/movies?id=1",function(data){
+  results = JSON.parse(data);
+});
+console.log(results);
