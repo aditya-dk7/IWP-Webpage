51085ff08382b307b3b5643ba2e5f327c647ad4c
Aditya <rameshaditya2001@gmail.com>
2021-04-27 20:03:23 +0530

    Added The generate movie page in sync with the API

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-var results = "";
-$.get("http://127.0.0.1:6767/api/v1/resources/movies?id=1",function(data){
-  results = JSON.parse(data);
-});
-console.log(results);
+function getMovieRecommendation(){
+    var movieName = document.getElementById("random__movie__name__");
+    var movieCast = document.getElementById("random__movie__cast__");
+    var movieYear = document.getElementById("random__movie__year__");
+    var movieImage = document.getElementById("random__movie__image__");
+    var movieRating = document.getElementById("random__movie__rating__");
+    var getSelectedGenre = document.getElementById("get__selected__item__");
+    url = "http://127.0.0.1:6767/api/v1/resources/movies/random";
+    if (getSelectedGenre.value != "Random Genre") {
+        url = "http://127.0.0.1:6767/api/v1/resources/movies/random?genre=" + getSelectedGenre.value;
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = function (e) {
+        if (this.status == 200) {
+            console.log(this.response);
+            var response = this.response
+            response.forEach((repo) => {
+                movieName.innerHTML = repo.title;
+                movieCast.innerHTML = repo.star;
+                movieYear.innerHTML = repo.year;
+                movieImage.src = repo.imgurl;
+                if(repo.rating){
+                    movieRating.innerHTML = repo.rating;
+                }else{
+                    movieRating.innerHTML = "";
+                }
+                
+              });          
+        }
+    };
+    xhr.send();
+    
+}
